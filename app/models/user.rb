d3c630f1cbea7446ef755560971b58f7ba60c316
kosimaru1997 <kosimaru1997@gmail.com>
2021-05-01 15:31:22 +0000

    応用課題３スタート

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
    
-  has_many :books
+  has_many :books, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
   attachment :profile_image
   
 end
