647fb2fa7a018dfe9d4b635bafd56c68ce9f061d
kosimaru1997 <kosimaru1997@gmail.com>
2021-05-01 18:54:32 +0000

    いいね機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :books do
+    resource :favorites, only: [:create, :destroy]
+    
     resources :book_comments, only: [:create, :destroy]
   end
   devise_for :users
