92486ae4d77846846f6ec7eaf91d2a6aa65bcece
kosimaru1997 <kosimaru1997@gmail.com>
2021-05-02 15:43:23 +0000

    フォロー、アソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,29 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  # フォロー
+  has_many :active_rerationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :following, through: :active_rerationships, source: :followed
+  # フォロワー
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
+  has_many :followers, through: :passive_relationships  #source: :follower
+
   attachment :profile_image
   
+  def follow(other_user)
+    following << other_user
+  end
+  
+  def unfollow(other_user)
+    active_rerationships.find_by(followed_id).destroy
+  end
+  
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   resources :books do
     resource :favorites, only: [:create, :destroy]
-    
     resources :book_comments, only: [:create, :destroy]
+    resources :relationships, only: [:create, :destroy]
+    member do
+      get :following, :followers
+    end
   end
+  
   devise_for :users
   root 'homes#top'
   get "/home/about" => "homes#about"
