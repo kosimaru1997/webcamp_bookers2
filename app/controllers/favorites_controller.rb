4c77aebc8cd5111fe46f3f073dbc48c643f3a7dd
kosimaru1997 <kosimaru1997@gmail.com>
2021-05-04 10:38:18 +0000

    応用課題５　修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,12 @@
 class FavoritesController < ApplicationController
   
   def create
-    favorite = current_user.favorites.build(book_id: params[:book_id])
-    favorite.save
+    current_user.favorites.create(book_id: params[:book_id])
     redirect_to request.referer
   end
 
   def destroy
-    favorite = current_user.favorites.find_by(book_id: params[:book_id])
-    favorite.destroy
+    current_user.favorites.find_by(book_id: params[:book_id]).destroy
     redirect_to request.referer
   end
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -39,7 +39,7 @@ class User < ApplicationRecord
   def self.looks(search, word)
     case search
       when "perfect_match"
-        @user = User.where("nam LIKE?", "#{word}")
+        @user = User.where("name LIKE?", "#{word}")
       when "forward_match"
         @user = User.where("name LIKE?", "#{word}%")
       when
