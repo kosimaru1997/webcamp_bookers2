f59bc88bdcf86dd7d0b5aca7f68b3db3be0363c1
kosimaru1997 <kosimaru1997@gmail.com>
2021-05-04 18:59:55 +0000

    いいね、コメントAjax

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,25 +1,29 @@
 class BookCommentsController < ApplicationController
   before_action :baria_user, only: [:destroy]
-  
+
   def create
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @book_comments = @book.book_comments
+    @book_comment = BookComment.new
+    # redirect_to book_path(book)
   end
-  
+
   def destroy
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+    @book_comments = @book.book_comments
   end
-  
+
   private
-   
+
    def book_comment_params
      params.require(:book_comment).permit(:comment)
    end
-   
+
   def baria_user
     unless BookComment.find_by(id: params[:id], book_id: params[:book_id]).user == current_user
       redirect_to books_path
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,15 @@
 class FavoritesController < ApplicationController
 
   def create
+    @book = Book.find(params[:book_id])
     current_user.favorites.create(book_id: params[:book_id])
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
     current_user.favorites.find_by(book_id: params[:book_id]).destroy
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
 end
